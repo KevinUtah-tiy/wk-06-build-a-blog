df1e39df2b2dc59001080fdc4ed91995cd83c382
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-31 19:40:22 -0600

    chore(init) Populate App.js, Header.js, index.js, index.html

---
+++ src/app/components/App.js
@@ -0,0 +1,9 @@
+fetch('./app/test.json')
+// make the request for the json file
+.then(function(response) {
+    // Convert to JSON
+    return response.json();
+}).then(function(obj) {
+    // Yay, `j` is a JavaScript object
+    console.log(obj);
+});
